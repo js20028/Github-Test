14ca0c50fb0ddd81b6a45cf92fea674e2dbaef72
곽재선 <mix2088@nate.com>
2022-09-14 15:06:05 +0900

    first commit

---
+++ GithubTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GithubTest
+//
+//  Created by 곽재선 on 2022/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Github TEST")
+    }
+
+
+}
+
